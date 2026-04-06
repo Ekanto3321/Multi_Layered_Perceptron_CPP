3745d0e91ff18cf650f13ab896f82063e638eaa4
Ekanto3321 <araddhaekanto@gmail.com>
2026-04-06 12:40:40 +0600

    nice output table for X-OR

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 #include <nn.h>
-#include "data/cat_tr.h"
-#include "data/car_tr.h"
-#include "data/apple_tr.h"
-#include "data/frog_tr.h"
-#include "data/banana_tr.h"
+//#include "data/cat_tr.h"
+// #include "data/car_tr.h"
+// #include "data/apple_tr.h"
+// #include "data/frog_tr.h"
+// #include "data/banana_tr.h"
+
+// includes for graphics
+#include <iostream>
+#include <iomanip>
 
 int input = 784;
 int hidden = 128;
 int output = 5;
+int N = 5000;
 uint8_t img[28][28];
 int r_int;
 double learning_rate = 0.1;
@@ -22,7 +27,7 @@ NeuralNetwork nn(input, hidden, output, learning_rate);
 int main() {
   
    
-    load();
+    // load();
     
     test();
     return 0;
@@ -31,17 +36,20 @@ int main() {
 
 
 
-void load(){
+// void load(){
 
-    for (int i = 0; i < 1000; i++)
-    {
-        /* code */
-    }
-    
+//     for (int i = 0; i < N; i++) {
+//         data_inp[i].resize(28, 28);  
+//     }
 
+//     for (int i = 0; i < 1000; i++) {
+//         Map<const Matrix<float, 28, 28, RowMajor>> m(&cat_tr[i][0][0]);
+//         data_inp[i] = m.cast<double>();
+//     }
 
+//     cout<<"data loaded successfully"<<endl;
+// }
 
-}
 
 
 
@@ -72,11 +80,28 @@ void test(){
     cout<<"Weights:\nW_IH\n"<<nn_t.W_IH<<"\nW_H\n"<<nn_t.W_HO<<endl; 
 
     cout<<"INFERENCE:\n";
-    for (int i = 0; i < 4; i++)
-    {
+    // for (int i = 0; i < 4; i++)
+    // {
+    //     nn_t.inputs = data_inp.row(i).transpose();
+    //     nn_t.target = data_t.row(i).transpose();
+    //     cout<<nn_t.inputs<<"\n\n"<<nn_t.think(nn_t.inputs)<<"\n\n";
+        
+    // }
+
+    std::cout << "Input 1 | Input 2 | Output\n";
+    std::cout << "--------------------------\n";
+
+    for (int i = 0; i < 4; ++i) {
         nn_t.inputs = data_inp.row(i).transpose();
         nn_t.target = data_t.row(i).transpose();
-        cout<<nn_t.inputs<<"\n\n"<<nn_t.think(nn_t.inputs)<<"\n\n";
         
-    }
+       double in1 = nn_t.inputs(0);
+        double in2 = nn_t.inputs(1);
+        double out = nn_t.think(nn_t.inputs)(0);
+
+        std::cout << std::setw(4) << in1 << "    | " 
+                << std::setw(4) << in2 << "    | " 
+                << std::setw(4) << out << "\n";
+}
+
 }
